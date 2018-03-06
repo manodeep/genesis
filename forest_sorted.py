e79ad044862fbfecfb52a093fd0f18895954847b
Jacob <jseiler@swin.edu.au>
2018-03-06 19:16:42 +1100

    Forest sorting script works properly (for now).
    I am correctly sorting/sub-sorting the properties on the ForestID/HaloMass.
    The script copies the hdf5 datasets to a new file and then switches the order that the properties are written to match the sorted order.
    TODO: Update docstrings for my functions and do a final pass of checks.

--- forest_sorted.py
+++ forest_sorted.py
@@ -113,14 +113,13 @@ def get_sorted_indices(dataset, snap_key, opt_args):
     indices = np.lexsort((dataset[snap_key][opt_args["sort_mass"]], dataset[snap_key][opt_args["sort_id"]])) # Sorts via the ID then sub-sorts via the mass
 
     if opt_args["debug"] == 1: 
-        status = check_sorted_indices(f[snap_key][halo_id], f[snap_key][sort_id], f[snap_key][sort_mass], indices, opt_args)
-        if (status == -1):
-            print("Error in the sorted indices using Snapshot Key {0}".format(snap_key))
-            exit()
+        test_sorted_indices(f[snap_key][halo_id], f[snap_key][sort_id], f[snap_key][sort_mass], indices, opt_args)
+
+        #assert(len(indices) == len(dataset[snap_key][opt_args["sort_id"]])) Put it in previous function.
 
     return indices
 
-def check_sorted_indices(halo_id, match_id, match_mass, indices, opt_args):
+def test_sorted_indices(halo_id, match_id, match_mass, indices, opt_args):
     """
 
     Checks the indices of the sorted array to ensure that the sorting has been performed properly. 
@@ -157,7 +156,7 @@ def check_sorted_indices(halo_id, match_id, match_mass, indices, opt_args):
 
             print("For Halo ID {0} we had a {4} of {1}.  After sorting via lexsort with key {4}, the next Halo has ID {2} and {4} of {3}.".format(halo_id[indices[idx]], match_id[indices[idx]], halo_id[indices[idx + 1]], match_id[indices[idx + 1]], opt_args["sort_id"]))
             print("Since we are sorting using {0} they MUST be in ascending order.".format(opt_args["sort_id"]))
-            return -1
+            #raise Blah 
 
         if (match_id[indices[idx]] == match_id[indices[idx + 1]]): # Then for the inner-sort, check that the sort within each ID group was done correctly.
             if (match_mass[indices[idx]] > match_mass[indices[idx]]):
@@ -165,39 +164,121 @@ def check_sorted_indices(halo_id, match_id, match_mass, indices, opt_args):
                 print("For Halo ID {0} we had a {4} of {1}.  After sorting via lexsort with key {4}, the next Halo has ID {2} and {4} of {3}.".format(halo_id[indices[idx]], match_id[indices[idx]], halo_id[indices[idx + 1]], match_id[indices[idx + 1]], opt_args["sort_id"]))
                 print("However we have sub-sorted within {0} with the key {1}.  Halo ID {2} has a {1} value of {3} whereas the sequentially next halo with ID {4} has a {1} value of {5}".format(opt_args["sort_id"], opt_args["sort_mass"], halo_id[indices[idx]], match_mass[indices[idx]], halo_id[indices[idx + 1]], mass[indices[idx + 1]])) 
                 print("Since we are sub-sorting using {0} they MUST be in ascending order.".format(opt_args["sort_mass"]))
-                return -1
+                #raise Blah 
 
-    return 0
 
-if __name__ == '__main__':
+def parse_snap_field(Snap_Field):
+
+    SnapNum = ""
+
+    for letter in Snap_Field:
+        if (letter.isdigit() == True): 
+            SnapNum = "{0}{1}".format(SnapNum, letter)
+
+    return int(SnapNum)
+
+
+def parse_inputs():
 
     parser = OptionParser()
 
-    parser.add_option("-f", "--fname", dest="fname", help="Path to the HDF5 data file. Required.")
-    parser.add_option("-s", "--forestID", dest="sort_id", help="Field name for the key we are sorting on. Default: ForestID.", default = "ForestID")
+    parser.add_option("-f", "--fname_in", dest="fname_in", help="Path to the input HDF5 data file. Required.")
+    parser.add_option("-o", "--fname_out", dest="fname_out", help="Path to the output HDF5 data file. Required.")
+    parser.add_option("-s", "--sort_id", dest="sort_id", help="Field name for the key we are sorting on. Default: ForestID.", default = "ForestID")
     parser.add_option("-m", "--mass_def", dest="sort_mass", help="Field name for the mass key we are sorting on. Default: Mass_200mean.", default = "Mass_200mean")
-    parser.add_option("-i", "--ID", dest="halo_id", help="Field name for halo ID. Default: ID.", default = "ID")
+    parser.add_option("-i", "--HaloID", dest="halo_id", help="Field name for halo ID. Default: ID.", default = "ID")
     parser.add_option("-d", "--debug", dest="debug", help="Set to 1 to toggle debug mode. Default: 0 (off).", default = 0)
+    parser.add_option("-p", "--ID_fields", dest="ID_fields", help="Field names for those that contain IDs.  Default: ('ID', 'Tail', 'Head', 'NextSubHalo', 'Dummy1', 'Dumm2').", default = ("ID", "Tail", "Head", "NextSubHalo", "Dummy", "Dummy")) 
 
     (opt, args) = parser.parse_args()
 
-    if (opt.fname == None):
+    if (opt.fname_in == None or opt.fname_out == None):
         parser.print_help()
         exit()
 
-    halo_id = opt.halo_id
-    print("The HaloID field for each halo is {0}.".format(halo_id)) 
-       
-    sort_id = opt.sort_id
-    print("Sorting on the {0} field.".format(sort_id))
+    print("The HaloID field for each halo is {0}.".format(opt.halo_id)) 
+    print("Sorting on the {0} field.".format(opt.sort_id))
+    print("Sub-Sorting on the {0} field.".format(opt.sort_mass))
 
-    sort_mass = opt.sort_mass
-    print("Sub-Sorting on the {0} field.".format(sort_mass))
+    return (opt, args)
 
-    with h5py.File(opt.fname, "r") as f:
+def ID_to_temporalID(index, SnapNum):
+
+    temporalID = SnapNum*int(1e12) + index + 1
+
+    return temporalID
+
+def test_check_haloIDs(snap_file, SnapNum, opt):
+
+    targetIDs = ID_to_temporalID(np.arange(len(snap_file[opt['halo_id']])), SnapNum)
+    haloIDs =snap_file[opt['halo_id']]
+    
+    if (np.array_equal(targetIDs, haloIDs) == False):
+        raise ValueError("The HaloIDs did not match the formula!")
+
+    print("Snapshot {0} passed check_haloIDs".format(SnapNum))
+
+
+def copy_field(file_in, file_out, snap_key, field, opt_args):
+
+    group_path = file_in[snap_key][field].parent.name
+    group_id = file_out.require_group(group_path)
+    name = "{0}/{1}".format(snap_key, field)
+    f_in.copy(name, group_id, name = field)
+
+    if opt.args['debug'] == 1:
+        print("Created field {0} for snap_key {1}".format(field, snap_key))
+ 
+if __name__ == '__main__':
+
+    (opt, args) = parse_inputs()
+
+    outfile = "/Users/100921091/Desktop/Genesis/my_test.hdf5"
+    with h5py.File(opt.fname_in, "r") as f_in,  h5py.File(opt.fname_out, "w") as f_out:
 
         oldID_to_newID = {} # Dictionary to go from the oldID to the newID.
+     
+        snap_fields = [] 
+        snap_nums = {} 
+        for field in list(f_in.keys()): # Want to generate a list of snapshot fields and the associate snapshot number.            
+            if (field.find("Snap") > -1 or field.find("snap") > -1): # .find returns -1 if the string is not found.
+                snap_fields.append(field) # Remember the name of the field. 
+                snap_nums[field] = parse_snap_field(field) # Find out what snapshot number the name corresponds to. 
         
-        for snap_key in list(f.keys()): # Go through each field within the file.
-            if (snap_key[0] == "S" or snap_key[0] == "s"): # Skip those fields that are not snapshot fields.
-                indices = get_sorted_indices(f, snap_key, vars(opt))
+        ID_maps = {}
+        snapshot_indices = {}
+ 
+        for snap_key in snap_fields:
+            print(snap_key)
+            if (len(f_in[snap_key][opt.halo_id]) == 0): # If there aren't any halos at this snapshot, move along.
+                continue 
+
+            #for field in f_in[snap_key]:
+            #    print(field in opt.ID_fields)
+
+            if opt.debug:
+                test_check_haloIDs(f_in[snap_key], snap_nums[snap_key], vars(opt))
+
+            indices = get_sorted_indices(f_in, snap_key, vars(opt)) # Get the indices that will correctly sort the snapshot dataset by ForestID and halo mass.
+            old_haloIDs = f_in[snap_key][opt.halo_id][:][indices] # Grab the HaloIDs in the sorted order.
+            new_haloIDs = ID_to_temporalID(np.arange(len(indices)), snap_nums[snap_key]) # Generate new IDs.                       
+            oldIDs_to_newIDs = dict(zip(old_haloIDs, new_haloIDs)) # Create a dictionary mapping between the old and new IDs.
+            
+            snapshot_indices[snap_key] = indices # Move the indices and the map dictionary to a dictionary keyed by the snapshot field. 
+            ID_maps[snap_key] = oldIDs_to_newIDs
+                                    
+        ## At this point we have the dictionaries that map the oldIDs to the newIDs in addition to the indices that control the sorting of the array. ##
+        ## At this point we can loop through all the fields within each halo halo within each snapshot and if the field is 'ID', 'Tail', etc, generate the new field using the oldID->newID map. ##
+        ## While going through each field, we will then write out the data into a new HDF5 file in the order specified by indices; write(read_file[field][indices]). ## 
+
+        for snap_key in snap_fields: # Loop through snapshots.            
+            for field in f_in[snap_key]: # Loop through each field 
+                copy_field(f_in, f_out, snap_key, field) # Copy the field into the output file. 
+                for idx in range(len(f_in[snap_key][opt.halo_id])): # Loop through each halo within the field.
+                    if (field in opt.ID_fields) == True: # If we need to update the ID for this field.                    
+                        oldID = f_in[snap_key][opt.halo_id][idx] 
+                        f_out[snap_key][field][idx] = ID_maps[snap_key][oldID] # Overwrite the oldID with the newID.  We can overwrite because we now 
+
+                if len(f_in[snap_key][opt.halo_id]) > 0:
+                    f_out[snap_key][field][:] = f_out[snap_key][field][:][snapshot_indices[snap_key]] # Then reorder the properties to be in the sorted order.
+ 
